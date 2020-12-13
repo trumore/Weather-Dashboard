79a120b61484129fcb4a5d5fc944806be453c2e0
Tom Rumore <rumoretom@gmail.com>
2020-12-13 15:30:57 -0500

    went back to start to log click event

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
-var APIKey = "19a0504277534924261213bbaabf5bbe";
-var city; //define in click event on search city to grab data from input
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" = 
+$(document).ready(function () {
+  $("#searchButton").on("click", function () {
+    alert("Click");
+  });
+});
