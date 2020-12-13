c4febf63d8ac4b717f4fd80af6a457cf4b997a9d
Tom Rumore <rumoretom@gmail.com>
2020-12-13 13:16:37 -0500

    defined api key variable with personal api key from openweathermap

---
+++ script.js
@@ -0,0 +1,3 @@
+var APIKey = "19a0504277534924261213bbaabf5bbe";
+var city; //define in click event on search city to grab data from input
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" = 
