b802d3e0845422e30b9ce628f43cf5786719cb43
Tom Rumore <rumoretom@gmail.com>
2020-12-17 11:33:04 -0500

    added VAR for openweathermap APIKey along with some pseudocode to create search function based on data attribute

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ $(document).ready(function () {
   $("#searchButton").on("click", function () {
     alert("Click");
   });
+
+  var APIKey = "19a0504277534924261213bbaabf5bbe";
+
+  //create var that links queryURL with API Key
+  //get data attribute from search click event to link with a function
+  //includes queryURL + "attr" + APIKey
 });
