34ebded8d8d15eba6175f4d35fdb27e20f07278b
Tom Rumore <rumoretom@gmail.com>
2020-12-19 16:41:28 -0500

    first ajax call and log to show result successful

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
   var cities = [];
-  var APIKey = "19a0504277534924261213bbaabf5bbe";
-
+  var city;
   function cityButtons() {
     $("#cities-list").empty();
     for (var i = 0; i < cities.length; i++) {
@@ -10,15 +9,25 @@ $(document).ready(function () {
       a.attr("city-name", cities[i]);
       a.text(cities[i]);
       $("#cities-list").append(a);
-      console.log("movie");
     }
   }
 
   $("#searchButton").on("click", function (event) {
     event.preventDefault();
-    var city = $("#city").val().trim();
+    var city = $("#city").val();
     cities.push(city);
     cityButtons();
+    var queryURL =
+      "https:api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=19a0504277534924261213bbaabf5bbe";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (response) {
+      console.log(response);
+    });
   });
   //create var that links queryURL with API Key
   //get data attribute from search click event to link with a function
