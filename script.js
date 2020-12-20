3b5a6ece4321ff8e91c7757dd683b6cd2b08650b
Tom Rumore <rumoretom@gmail.com>
2020-12-19 19:58:34 -0500

    added second ajax call to reference 5 day forecast which would also display icons for forecasted days git status git add script.js

--- script.js
+++ script.js
@@ -62,6 +62,10 @@ $(document).ready(function () {
       var futureTempCalc = response.list[0].main.temp * (9 / 5) - 459.67;
       $(".day1temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
       $(".day1hum").text("Humidity: " + response.list[0].main.humidity + "%");
+      var day1Slide = response.list[0].weather[0].icon;
+      var getday1Image =
+        "http://openweathermap.org/img/wn/" + day1Slide + "@2x.png";
+      $("#day1image").attr("src", getday1Image);
 
       var getDate2 = response.list[1].dt_txt;
       var showDate2 =
@@ -74,6 +78,10 @@ $(document).ready(function () {
       var futureTempCalc = response.list[1].main.temp * (9 / 5) - 459.67;
       $(".day2temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
       $(".day2hum").text("Humidity: " + response.list[1].main.humidity + "%");
+      var day2Slide = response.list[1].weather[0].icon;
+      var getday2Image =
+        "http://openweathermap.org/img/wn/" + day2Slide + "@2x.png";
+      $("#day2image").attr("src", getday2Image);
 
       var getDate3 = response.list[2].dt_txt;
       var showDate3 =
@@ -86,6 +94,10 @@ $(document).ready(function () {
       var futureTempCalc = response.list[2].main.temp * (9 / 5) - 459.67;
       $(".day3temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
       $(".day3hum").text("Humidity: " + response.list[2].main.humidity + "%");
+      var day3Slide = response.list[2].weather[0].icon;
+      var getday3Image =
+        "http://openweathermap.org/img/wn/" + day3Slide + "@2x.png";
+      $("#day3image").attr("src", getday3Image);
 
       var getDate4 = response.list[3].dt_txt;
       var showDate4 =
@@ -98,6 +110,10 @@ $(document).ready(function () {
       var futureTempCalc = response.list[3].main.temp * (9 / 5) - 459.67;
       $(".day4temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
       $(".day4hum").text("Humidity: " + response.list[3].main.humidity + "%");
+      var day4Slide = response.list[3].weather[0].icon;
+      var getday4Image =
+        "http://openweathermap.org/img/wn/" + day4Slide + "@2x.png";
+      $("#day4image").attr("src", getday4Image);
 
       var getDate5 = response.list[4].dt_txt;
       var showDate5 =
@@ -110,6 +126,10 @@ $(document).ready(function () {
       var futureTempCalc = response.list[4].main.temp * (9 / 5) - 459.67;
       $(".day5temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
       $(".day5hum").text("Humidity: " + response.list[4].main.humidity + "%");
+      var day5Slide = response.list[4].weather[0].icon;
+      var getday5Image =
+        "http://openweathermap.org/img/wn/" + day5Slide + "@2x.png";
+      $("#day5image").attr("src", getday5Image);
     });
   });
   //create var that links queryURL with API Key
