c9bf05788969d8b5fa4c3d92c25da0ec995301e7
Tom Rumore <rumoretom@gmail.com>
2020-12-19 22:55:37 -0500

    added date display functionality for 5 day forecast

--- script.js
+++ script.js
@@ -51,85 +51,86 @@ $(document).ready(function () {
       method: "GET",
     }).then(function (response) {
       console.log(response);
-      var getDate1 = response.list[0].dt_txt;
-      var showDate1 =
-        getDate1.charAt(5) +
-        getDate1.charAt(6) +
-        "/" +
-        getDate1.charAt(8) +
-        getDate1.charAt(9);
-      $("day1date").text("Date" + showDate1);
-      var futureTempCalc = response.list[0].main.temp * (9 / 5) - 459.67;
+      var getDate1 = luxon.DateTime.fromSeconds(
+        response.list[0].dt
+      ).toLocaleString({
+        month: "short",
+        day: "2-digit",
+      });
+      console.log(getDate1);
+      $(".day1date").text("Date: " + getDate1);
+      var futureTempCalc = response.list[2].main.temp * (9 / 5) - 459.67;
       $(".day1temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
-      $(".day1hum").text("Humidity: " + response.list[0].main.humidity + "%");
-      var day1Slide = response.list[0].weather[0].icon;
+      $(".day1hum").text("Humidity: " + response.list[2].main.humidity + "%");
+      var day1Slide = response.list[2].weather[0].icon;
       var getday1Image =
         "http://openweathermap.org/img/wn/" + day1Slide + "@2x.png";
       $("#day1image").attr("src", getday1Image);
 
-      var getDate2 = response.list[1].dt_txt;
-      var showDate2 =
-        getDate2.charAt(5) +
-        getDate2.charAt(6) +
-        "/" +
-        getDate2.charAt(8) +
-        getDate2.charAt(9);
-      $("day2date").text("Date" + showDate2);
-      var futureTempCalc = response.list[1].main.temp * (9 / 5) - 459.67;
+      var getDate2 = luxon.DateTime.fromSeconds(
+        response.list[10].dt
+      ).toLocaleString({
+        month: "short",
+        day: "2-digit",
+      });
+      $(".day2date").text("Date: " + getDate2);
+      var futureTempCalc = response.list[10].main.temp * (9 / 5) - 459.67;
       $(".day2temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
       $(".day2hum").text("Humidity: " + response.list[1].main.humidity + "%");
-      var day2Slide = response.list[1].weather[0].icon;
+      var day2Slide = response.list[10].weather[0].icon;
       var getday2Image =
         "http://openweathermap.org/img/wn/" + day2Slide + "@2x.png";
       $("#day2image").attr("src", getday2Image);
 
-      var getDate3 = response.list[2].dt_txt;
-      var showDate3 =
-        getDate3.charAt(5) +
-        getDate3.charAt(6) +
-        "/" +
-        getDate3.charAt(8) +
-        getDate3.charAt(9);
-      $("day3date").text("Date" + showDate3);
-      var futureTempCalc = response.list[2].main.temp * (9 / 5) - 459.67;
+      var getDate3 = luxon.DateTime.fromSeconds(
+        response.list[18].dt
+      ).toLocaleString({
+        month: "short",
+        day: "2-digit",
+      });
+      $(".day3date").text("Date: " + getDate3);
+      var futureTempCalc = response.list[18].main.temp * (9 / 5) - 459.67;
       $(".day3temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
-      $(".day3hum").text("Humidity: " + response.list[2].main.humidity + "%");
-      var day3Slide = response.list[2].weather[0].icon;
+      $(".day3hum").text("Humidity: " + response.list[18].main.humidity + "%");
+      var day3Slide = response.list[18].weather[0].icon;
       var getday3Image =
         "http://openweathermap.org/img/wn/" + day3Slide + "@2x.png";
       $("#day3image").attr("src", getday3Image);
 
-      var getDate4 = response.list[3].dt_txt;
-      var showDate4 =
-        getDate4.charAt(5) +
-        getDate4.charAt(6) +
-        "/" +
-        getDate4.charAt(8) +
-        getDate4.charAt(9);
-      $("day4date").text("Date" + showDate4);
-      var futureTempCalc = response.list[3].main.temp * (9 / 5) - 459.67;
+      var getDate4 = luxon.DateTime.fromSeconds(
+        response.list[26].dt
+      ).toLocaleString({
+        month: "short",
+        day: "2-digit",
+      });
+      $(".day4date").text("Date: " + getDate4);
+      var futureTempCalc = response.list[26].main.temp * (9 / 5) - 459.67;
       $(".day4temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
-      $(".day4hum").text("Humidity: " + response.list[3].main.humidity + "%");
-      var day4Slide = response.list[3].weather[0].icon;
+      $(".day4hum").text("Humidity: " + response.list[26].main.humidity + "%");
+      var day4Slide = response.list[26].weather[0].icon;
       var getday4Image =
         "http://openweathermap.org/img/wn/" + day4Slide + "@2x.png";
       $("#day4image").attr("src", getday4Image);
 
-      var getDate5 = response.list[4].dt_txt;
-      var showDate5 =
-        getDate5.charAt(5) +
-        getDate5.charAt(6) +
-        "/" +
-        getDate5.charAt(8) +
-        getDate5.charAt(9);
-      $("day5date").text("Date" + showDate4);
-      var futureTempCalc = response.list[4].main.temp * (9 / 5) - 459.67;
+      var getDate5 = luxon.DateTime.fromSeconds(
+        response.list[34].dt
+      ).toLocaleString({
+        month: "short",
+        day: "2-digit",
+      });
+      $(".day5date").text("Date: " + getDate5);
+      var futureTempCalc = response.list[34].main.temp * (9 / 5) - 459.67;
       $(".day5temp").text("Temp: " + futureTempCalc.toFixed(1) + "°F");
-      $(".day5hum").text("Humidity: " + response.list[4].main.humidity + "%");
-      var day5Slide = response.list[4].weather[0].icon;
+      $(".day5hum").text("Humidity: " + response.list[34].main.humidity + "%");
+      var day5Slide = response.list[34].weather[0].icon;
       var getday5Image =
         "http://openweathermap.org/img/wn/" + day5Slide + "@2x.png";
       $("#day5image").attr("src", getday5Image);
     });
+  });
+
+  $("#clearButton").on("click", function () {
+    $("#cities-list").remove();
+    console.log("click");
   });
 });
